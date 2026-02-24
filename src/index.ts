0e123881bddba42a913bc2e44921824119ad1cbe
Mizael Almeida <mizael.almeida@salvy.com.br>
2026-02-24 14:03:39 -0300

    feat: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "dotenv/config";
 import { getConfig, isConfigured } from "./config.js";
 import { verifyWebhook, type SvixHeaders } from "./verify.js";
 import { sendMessage } from "./telegram.js";
